002e385083be4a37da360db5279c08f7915c1f5c
Schwyn Francis <schwynf@gmail.com>
2020-08-07 22:09:47 -0700

    added more routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,14 @@ const passport = require("passport")
 require("../config/passport")(passport);
 
 
-// Defining methods for the ProfilesController
 module.exports = {
+  //Create User
   createUser: function (req, res, next) {
     passport.authenticate('jwt', async function (err, mongoUser, info) {
       if (mongoUser) {
         console.log("Access granted for user with JWT")
         db.User
-          .create({email:req.body.email})
+          .create({ email: req.body.email })
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
       } else {
@@ -19,12 +19,29 @@ module.exports = {
       }
     })(req, res, next);
   },
-  findAll: function (req, res, next) {
+  //Find User
+  findUser: function (req, res, next) {
+    passport.authenticate('jwt', async function (err, mongoUser, info) {
+      if (mongoUser) {
+        console.log(mongoUser)
+        console.log("Access granted for user with JWT")
+        db.User
+          .find({ email: mongoUser[0].email })
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      } else {
+        console.log("Access denied for user with JWT")
+        res.json({ loggedIn: false })
+      }
+    })(req, res, next);
+  },
+  //Add Project to User
+  addProject: function (req, res, next) {
     passport.authenticate('jwt', async function (err, mongoUser, info) {
       if (mongoUser) {
         console.log("Access granted for user with JWT")
         db.User
-          .find({email:req.body.email})
+          .update({}, {})
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
       } else {
@@ -33,4 +50,4 @@ module.exports = {
       }
     })(req, res, next);
   }
-};
+}
